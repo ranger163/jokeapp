8dd6046a5a36f6df4dad2dcc46562a0f9d1d5bc8
ranger163 <ahmed.nassar163@gmail.com>
2020-05-16 17:55:05 +0200

    Adding JokeService interface

---
+++ src/main/java/me/inassar/jokeapp/services/JokeService.java
@@ -0,0 +1,12 @@
+package me.inassar.jokeapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+/**
+ * Created by Ahmed Nassar.
+ * Date: 16 May, 2020
+ * Time: 5:51 PM
+ */
+public interface JokeService {
+    String getRandomJoke();
+}
